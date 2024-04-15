7a7c686cb024ab69fd918a6e35c3cd41bf4310fd
SebastianCA19 <castroarrietasebastiandavid@gmail.com>
2024-04-14 19:42:17 -0500

    Muestra de resultados, arreglo de errores y mejoras en el css

--- public/main.js
+++ public/main.js
@@ -1,14 +1,16 @@
 var originalDocumentation = null;
+var tokens = null;
+var tokensJSON = null;
 
 function getInput(){
     var code = document.getElementById("inputCode").value;
+    code = code.replace(/[\r\n]+/g, ' ');
     sendCode(code);
 }
 
 function sendCode(code) {
     if(originalDocumentation == null){
         originalDocumentation = document.querySelector(".results-content").textContent;
-        console.log(originalDocumentation);
     }
     fetch('http://127.0.0.1:5000/analyzeCode', {
         method: 'POST',
@@ -30,8 +32,10 @@ function sendCode(code) {
         if (data.error) {
             alert('Error en el análisis del código: ' + data.error);
         } else {
-            var textarea = document.getElementById("documentation");
-            textarea.value = data.result;
+            tokensJSON = JSON.stringify(data);
+            tokens = JSON.parse(tokensJSON);
+            console.log(tokens);
+            showTokens(tokens);
         }
     })
     .catch(error => {
@@ -39,6 +43,29 @@ function sendCode(code) {
         alert('Ocurrió un error al procesar el código');
     });
 }
+
+function showTokens(tokensJSON){
+    var json = JSON.parse(JSON.stringify(tokensJSON));
+    var tableResults = null;
+
+    tableResults = 'Valor\tToken\n';
+    Object.keys(json).forEach(index => {
+        if(json[index].token == undefined){
+            tableResults = `Hay un error en el código.\nDetalles: ${json[index].error} ${json[index].details}\n`
+        }else{
+            let token = json[index].token;
+            let value = json[index].value;
+    
+            // Agregar cada token al string de la tabla
+            tableResults += `${value}\t\t${token}\n`;
+        }
+    });
+
+    // Mostrar el string de la tabla en el textarea
+    var resultsTextarea = document.getElementById("documentation");
+    resultsTextarea.value = tableResults;
+}
+
 
 function back(){
     document.querySelector(".results-content").value = originalDocumentation;
--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import lexer
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -11,9 +12,13 @@ def analyze_code():
         code = request.json['codeInput']
         result, err = lexer.run(code)
         if err:
-            return jsonify(error=err), 400
+            errJSON = {}
+            errJSON[0]= {
+                'error' : err.error_name, 'details' : err.details
+            }
+            return json.dumps(errJSON)
         else:
-            return jsonify(result=result), 200
+            return json.dumps(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
